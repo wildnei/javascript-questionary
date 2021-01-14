f03ff906ed93b8d48e75be49e933218353a63447
wildnei <wildneisuane@gmail.com>
2021-01-14 00:40:19 -0500

    finished skeleton with no bugs

--- assets/end.js
+++ assets/end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = document.querySelector('mostRecentScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
@@ -30,7 +30,7 @@ saveHighScore = e => {
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
-    
+    window.location.assign('highscores.html')
+
 }
 
--- assets/highscores.js
+++ assets/highscores.js
@@ -1,7 +1,7 @@
 const highScoreList = document.querySelector('#highScoresList')
-const HighSCores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 highScoresList.innerHTML = 
 highScores.map(score => {
-    return '<li class="high-score">${score.name} - ${score.score}</li>'
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join('')
