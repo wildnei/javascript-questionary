f376bb3b49d1ad628de01e85efa878bae8b64ab8
wildnei <wildneisuane@gmail.com>
2021-01-14 10:44:43 -0500

    clear button now erases local storage

--- assets/end.js
+++ assets/end.js
@@ -30,7 +30,7 @@ saveHighScore = e => {
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('highscores.html')
+    window.location.assign('highScores.html')
 
 }
 
--- assets/highscores.js
+++ assets/highscores.js
@@ -9,6 +9,12 @@ highScores.map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join('')
 
+
 $("#clear").on("click", function (event) {
     $("#highScoresList").empty();
+});
+
+
+$("#clear").on("click", function (event) {
+    window.localStorage.clear();
 });
